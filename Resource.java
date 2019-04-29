8fe5f7e1cb63553bf24e60a185524579ff108d21
Macusmph <44886174+Macusmph@users.noreply.github.com>
2019-04-29 18:18:56 -0500

    Update Resource.java

--- Resource.java
+++ Resource.java
@@ -4,44 +4,4 @@ public abstract class Resource {
     private String currentStatus;
     private String regionOfOrigin;
     private String responsibleOfTheFinding;
-    
-    public void setTextualContent(String textualContent){
-        this.textualContent = textualContent;
-    }
-    
-    public String getTextualContent(){
-        return textualContent;
-    }
-    
-    public void setLanguage(String language){
-        this.language = language;
-    }
-    
-    public String getLanguage(){
-        return language;
-    }
-    
-    public void setCurrentStatus(String currentStatus){
-        this.currentStatus = currentStatus;
-    }
-    
-    public String getCurrentStatus(){
-        return currentStatus;
-    }
-    
-    public void setRegionOfOrigin(String regionOfOrigin){
-        this.regionOfOrigin = regionOfOrigin;
-    }
-    
-    public String getRegionOfOrigin(){
-        return regionOfOrigin;
-    }
-    
-    public void setResponsibleOfTheFinding(String responsibleOfTheFinding){
-        this.responsibleOfTheFinding = responsibleOfTheFinding;
-    }
-    
-    public String getResponsibleOfTheFinding(){
-        return responsibleOfTheFinding;
-    }
 }
